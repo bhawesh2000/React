79fc51b8d3d2219084341a21d8de1acf38c9a7ee
Bhawesh <joshibhawesh502@gmail.com>
2023-08-01 02:31:37 +0530

    Fragments to remove extra node

---
+++ src/Components/Column.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Column() {
+  return (
+    <> {/* <> or <React.Fragment> is fragment helps to remove extra node so we replace div tag with fragment */}
+    <td>Name</td>
+    <td>Bhawesh</td>
+    </>
+  )
+}
+
+export default Column
